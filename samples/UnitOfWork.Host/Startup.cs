53c8523e14927674a0b4724b6f640625dae3f082
ruifeng.chang <ruifeng.chang@geely.com>
2020-08-18 10:07:18 +0800

    upgrade

--- samples/UnitOfWork.Host/Startup.cs
+++ samples/UnitOfWork.Host/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
+using log4net.Config;
+using log4net;
+using System.IO;
 
 namespace Arch.EntityFrameworkCore.UnitOfWork.Host
 {
@@ -22,6 +25,10 @@ namespace Arch.EntityFrameworkCore.UnitOfWork.Host
             {
             }
 
+            //加载log4net日志配置文件
+            var repository = LogManager.CreateRepository("NETCoreRepository");
+            XmlConfigurator.Configure(repository, new FileInfo("log4net.config"));
+
             builder.AddEnvironmentVariables();
             Configuration = builder.Build();
         }
@@ -33,21 +40,32 @@ namespace Arch.EntityFrameworkCore.UnitOfWork.Host
         {
             // use in memory for testing.
             services
-                .AddDbContext<BloggingContext>(opt => opt.UseMySql("Server=localhost;database=uow;uid=root;pwd=root1234;"))
-                //.AddDbContext<BloggingContext>(opt => opt.UseInMemoryDatabase("UnitOfWork"))
+                //.AddDbContext<BloggingContext>(opt => opt.UseMySql("Server=localhost;database=uow;uid=root;pwd=root1234;"))
+                .AddDbContext<BloggingContext>(opt => opt.UseInMemoryDatabase("UnitOfWork"))
                 .AddUnitOfWork<BloggingContext>()
                 .AddCustomRepository<Blog, CustomBlogRepository>();
+
+            services.AddLogging(conf =>
+            {
+                conf.AddFilter("System", LogLevel.Warning);
+                conf.AddFilter("Microsoft", LogLevel.Warning);
+                conf.AddLog4Net();
+            });
 
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggingBuilder loggingBuilder)
+        public void Configure(IApplicationBuilder app,
+            IWebHostEnvironment env,
+            //ILoggingBuilder loggingBuilder,
+            ILoggerFactory loggerFactory)
         {
-            loggingBuilder.AddConsole();
-            loggingBuilder.AddDebug();
+            loggerFactory.AddLog4Net();
 
-            app.UseEndpoints(endpoints => {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
                 endpoints.MapDefaultControllerRoute();
             });
         }
